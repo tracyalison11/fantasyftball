e1127782ef2aa41a3985d63b36eb3be5bfd4b86e
tracyalison11 <tracyak13@gmail.com>
2014-07-18 16:03:41 -0700

    first commit

--- app/js/config.js
+++ app/js/config.js
@@ -21,6 +21,6 @@ app.config(['$routeProvider',
 
   // your Firebase URL goes here
   // should look something like: https://blahblahblah.firebaseio.com
-  .constant('FBURL', '<<<FIREBASE_URL_GOES_HERE>>>')
+  .constant('FBURL', 'https://fantasyftball.firebaseio.com/')
 
 
